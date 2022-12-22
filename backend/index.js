0ff34369229405e7cb9a6a7679b192afee0648fc
tranquiltort <chrisreg007@gmail.com>
2022-12-22 14:36:09 -0500

    typescript react boiler plate with breweries logging to console

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,32 @@
 const express = require("express");
+const axios = require('axios');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+let brewList = null;
+
+axios.get('https://api.openbrewerydb.org/breweries')
+    .then(response =>{
+        console.log(response.data)
+        brewList= response.data;
+        // app.get("/api", (req, res) => {
+        //     res.json(brewList);
+        //   });
+        
+        // app.listen(PORT, () => {
+        //   console.log(`Server listening on ${PORT}`);
+        // });
+    })
+    .catch(error => {
+        console.log(error)
+    });
+
+
+app.get("/api", (req, res) => {
+    res.json(brewList);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
